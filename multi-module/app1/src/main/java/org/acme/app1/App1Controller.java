092407a9735cee4b8a4aea3d9c6d26a40f136906
Jeremie Bresson <jeremie.bresson@unblu.com>
2023-08-24 17:23:28 +0200

    Reproducer with kotlin

--- multi-module/app1/src/main/java/org/acme/app1/App1Controller.java
+++ multi-module/app1/src/main/java/org/acme/app1/App1Controller.java
@@ -1,6 +1,7 @@
 package org.acme.app1;
 
 import org.acme.core.CoreProcessor;
+import org.acme.core.kt.ConfigService;
 
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
@@ -14,9 +15,12 @@ public class App1Controller {
 	@Inject
 	private CoreProcessor processor;
 
+	@Inject
+	private ConfigService config;
+
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String hello() {
-		return "Messages:\n" + String.join("\n", processor.getAllMessages()) + "\n" + "-- end";
+		return config.getHeader() + ":\n" + String.join("\n", processor.getAllMessages()) + "\n" + "-- end";
 	}
 }
