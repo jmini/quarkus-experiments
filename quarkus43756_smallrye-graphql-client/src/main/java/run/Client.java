7e357617921bfc9b02e2c95352ca35c0fa1d84a2
Jeremie Bresson <jeremie.bresson@unblu.com>
2024-10-11 13:08:24 +0200

    Add and configure jboss-logger

--- quarkus43756_smallrye-graphql-client/src/main/java/run/Client.java
+++ quarkus43756_smallrye-graphql-client/src/main/java/run/Client.java
@@ -2,14 +2,20 @@ package run;
 
 import java.util.List;
 
+import org.jboss.logging.Logger;
+
 import io.smallrye.graphql.client.typesafe.api.TypesafeGraphQLClientBuilder;
 import superheroes.api.SuperheroesClientApi;
 import superheroes.model.City;
 import superheroes.model.Superhero;
 
 public class Client {
+	private static final Logger LOGGER = Logger.getLogger(Client.class);
+	
     public static void main(String... args) throws Exception {
-        
+        LOGGER.info("--- Start ---");
+        LOGGER.debug("Timestamp: " + System.currentTimeMillis());
+
         SuperheroesClientApi api = TypesafeGraphQLClientBuilder.newBuilder().build(SuperheroesClientApi.class);
 
         List<City> allCities = api.allCities();
@@ -17,6 +23,7 @@ public class Client {
         
         List<Superhero> allSuperheroes = api.allSuperheroes();
         System.out.println("response: " + allSuperheroes);
-        
+
+        LOGGER.info("--- End ---");
     }
 }
